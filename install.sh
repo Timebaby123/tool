e63d71a3cabf9472c281550f5f145d5c1a79b6ee
Timebaby123 <84785972+Timebaby123@users.noreply.github.com>
2021-05-25 21:09:37 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BLACK='\e[30m'
+RED='\e[31m'
+GREEN='\e[92m'
+YELLOW='\e[33m'
+ORANGE='\e[93m'
+BLUE='\e[34m'
+PURPLE='\e[35m'
+CYAN='\e[36m'
+WHITE='\e[37m'
+NC='\e[0m'
+PURPLE='\033[35m'
+       
+echo "Hacking Tools  ";                                       
+ 
+sudo apt install git
+wget http://github.com/Timebaby123/tools;
